1f1fe05cd8a5961d72ce0c3b97d1fc96fa520cc0
Zach Ickes <zachickes@hotmail.com>
2021-08-07 10:57:42 -0600

    added draw condition, and game loop, working on victory

--- game.rb
+++ game.rb
@@ -9,9 +9,21 @@ class Game
     puts "Sweet! #{player1.name} you're playing X's! who's going to play O's?"
     @player2 = Player.new(gets.chomp)
     puts 'Awesome! Have fun you two!!'
+    game_start
+  end
+
+  def game_start
     @board = Board.new
     @board.build_board
     player1_turn
+  end
+
+  def new_game(player1, player2)
+    @player1 = player2
+    @player2 = player1
+    puts "Another round? this time #{@player1.name} is going first hit enter to start!"
+    gets
+    game_start
   end
 
   def is_valid?(choice)
@@ -24,10 +36,16 @@ class Game
 
   def player1_turn
     puts "#{player1.name} Choose where you would like to go in the grid!"
-    choice = gets.chomp.to_i
+    choice = (gets.chomp.to_i - 1)
     if is_valid?(choice)
       @board.update(choice, player1.player_value)
+      if victory?
+        game_end(player1)
+      elsif self.game_end_tie?
+        self.game_end('tie')
+      else
       player2_turn
+      end
     else
       puts 'Invalid selection, please try again!'
       @board.display
@@ -37,14 +55,52 @@ class Game
 
   def player2_turn
     puts "#{player2.name} Choose where you would like to go in the grid!"
-    choice = gets.chomp.to_i
+    choice = (gets.chomp.to_i - 1)
     if is_valid?(choice)
       @board.update(choice, player2.player_value)
+      if victory?
+        game_end(player2)
+      elsif self.game_end_tie?
+        self.game_end('tie')
+      else
       player1_turn
+      end
     else
       puts 'Invalid selection, please try again!'
       @board.display
       player2_turn
+    end
+  end
+
+  def game_end_tie?
+    if @board.positions.include?(' ')
+      return false
+    else
+      return true
+    end
+  end
+
+  def victory?
+    false
+  end
+
+  def game_end(condition)
+    if condition == 'tie'
+      puts "It's a tie! good game you two, enter 'y' if you want to play another round!"
+      choice = gets.chomp
+      if choice.chomp.downcase == 'y'
+        self.new_game(player1, player2)
+      else
+        puts "Thanks for playing!"
+      end
+    else
+      puts "#{condition.name} won! Want to play again? Enter 'y' if you do!"
+      choice = gets.chomp
+      if choice.chomp.downcase == 'y'
+        self.new_game(player1, player2)
+      else
+        puts "Thanks for playing!"
+      end
     end
   end
 end
--- player.rb
+++ player.rb
@@ -1,18 +1,15 @@
 # class for the players
 class Player
   @@num_of_players = 0
-  attr_accessor :num_of_players, :player_value, :name
+  attr_reader :num_of_players, :player_value, :name
   def initialize(name)
     @name = name
-    if @@num_of_players == 0
+    if @@num_of_players.even?
       @player_value = 'X'
     else
       @player_value = 'O'
     end
     @@num_of_players += 1
-  end
-  def to_s
-    "Players: #{@@num_of_players} #{@player_value} player is #{name}"
   end
 end
 
